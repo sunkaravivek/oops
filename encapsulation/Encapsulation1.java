8ba1fdb40b058ec609e5a46d8e4fe50532d0ab0f
sunkaravivek <sunkaravivek2006@gmail.com>
2025-03-25 16:46:53 +0530

    Add files via upload

--- encapsulation/Encapsulation1.java
+++ encapsulation/Encapsulation1.java
@@ -1,25 +1,23 @@
 class Person {
-    private String name;  // Private variable
+    private String name;  
     private int age;
 
-    // Public getter method for name
-    public String getName() {
+        public String getName() {
         return name;
     }
 
-    // Public setter method for name
     public void setName(String newName) {
         this.name = newName;
     }
 
-    // Public getter method for age
+    
     public int getAge() {
         return age;
     }
 
-    // Public setter method for age
+    
     public void setAge(int newAge) {
-        if (newAge > 0) { // Ensuring age is positive
+        if (newAge > 0) { 
             this.age = newAge;
         } else {
             System.out.println("Age must be positive!");
@@ -27,16 +25,14 @@ class Person {
     }
 }
 
-// Main class
 public class Encapsulation1 {
     public static void main(String[] args) {
         Person person = new Person();
 
-        // Setting values using setter methods
+        
         person.setName("roopesh");
         person.setAge(19);
 
-        // Getting values using getter methods
         System.out.println("Name: " + person.getName());
         System.out.println("Age: " + person.getAge());
     }
