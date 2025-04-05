e5daa8b965db668f64259c68e20dcbbeee5fa0e1
sunkaravivek <sunkaravivek2006@gmail.com>
2025-04-05 21:00:02 +0530

    Create BuiltinPackage2.java

---
+++ Package/BuiltinPackage2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("\n=== Arithmetic Operations ===");
+        System.out.println("Addition: " + (num1 + num2));
+        System.out.println("Subtraction: " + (num1 - num2));
+        System.out.println("Multiplication: " + (num1 * num2));
+
+        if (num2 != 0) {
+            System.out.println("Division: " + (num1 / num2));
+        } else {
+            System.out.println("Division: Cannot divide by zero!");
+        }
+
+        sc.close();
+    }
+}
